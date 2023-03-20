cadbab6f85ad2a96ed8d40e643b1fc946f52da95
SanjaySinha <sanjayk.sinha@nic.in>
2023-03-20 16:53:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import streamlit as st
-import os
-import pathlib
-from os import listdir
-from os.path import isfile, join
-st.write("""
-# Demo
-""")
-parent_path = pathlib.Path(__file__).parent.parent.resolve()
-data_path = os.path.join(parent_path, "data")
-onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
-option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
-file_location=os.path.join(data_path, option)
-# use `file_location` as a parameter to the main script
+import pandas as pd
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+import streamlit as st
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)    
