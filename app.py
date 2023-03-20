3f3cce5e2823c5be62d5fd199adceb9c8a853f74
SanjaySinha <sanjayk.sinha@nic.in>
2023-03-20 17:11:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,6 @@ if uploaded_file is not None:
     #st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
+    df = pd.read_csv(uploaded_file)
     st.write(dataframe)
+st.dataframe(df, use_container_width=True)
