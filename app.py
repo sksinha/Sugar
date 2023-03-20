98b2c3ace3ce0820f8fdf1a301b204ea4f668e9f
SanjaySinha <sanjayk.sinha@nic.in>
2023-03-20 16:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,4 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
-import streamlit as st
-
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)    
+   
