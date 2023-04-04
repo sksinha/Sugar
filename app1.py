e4e331a1d9b8217c28ea441873a7bbc80f2623c4
SanjaySinha <sanjayk.sinha@nic.in>
2023-04-04 17:43:35 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -32,7 +32,6 @@ use_defo = st.checkbox('Use example Dataset')
 if use_defo:
     dataset = pd.read_csv('https://raw.githubusercontent.com/AnshuTrivedi/Data-Scientist-In-Python/master/Datasets/step_1/1.Python%20for%20data%20science%20fundamentals/googleplaystore.csv')
 
-
 st.sidebar.header('Import Dataset to Use Available Features: 👉')
 
 if dataset:
